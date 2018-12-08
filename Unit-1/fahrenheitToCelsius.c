62bf0f368cb7ded4e91d8308b7c64f0a9a0f87a8
Mikhail Patseyuk <mikhail.patseyuk@pandadoc.com>
2018-12-08 16:30:39 +0300

    Unit-1 [task-3]: cycle while, add header to the table

--- Unit-1/fahrenheitToCelsius.c
+++ Unit-1/fahrenheitToCelsius.c
@@ -3,17 +3,27 @@
 main() {
   int from, to, step;
   float fahrenheit, celsius;
+  char separator;
 
+  separator = '|';
   from = 0;
   to = 300;
   step = 20;
 
   fahrenheit = from;
 
+  printf("--------------------\n");
+  printf("|   F   |    C     |\n");
+
   while (fahrenheit <= to) {
     celsius = (5.0 / 9.0) * (fahrenheit - 32.0);
-    printf("%.0f\t %.2f\n", fahrenheit, celsius);
+    printf("--------------------\n");
+    printf("%c  %3.0f  %c  %6.2f  %c\n", separator, fahrenheit, separator, celsius, separator);
 
     fahrenheit = fahrenheit + step;
+
+    if (fahrenheit > to) {
+      printf("--------------------\n");
+    }
   }
 }
